9ee327e5710fba21b6b2dbe9ad0848f2307a27ce
Agne Rauluseviciute <63750622+agnerau@users.noreply.github.com>
2026-05-05 12:59:15 +0300

    create handler

---
+++ internal/handler.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PlantHandler struct {
+	Service *PlantService
+}
+
+func (h *PlantHandler) CreatePlant(c *gin.Context) {
+	var plant Plant
+
+	if err := c.ShouldBindJSON(&plant); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := h.Service.CreatePlant(&plant)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusCreated)
+}
+
+func (h *PlantHandler) GetPlants(c *gin.Context) {
+	plants, err := h.Service.GetPlants()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, plants)
+}
