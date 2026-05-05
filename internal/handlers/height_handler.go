b5b9ed67d5ee46f8a2d9ad678885240c4f4a2ef7
Agne Rauluseviciute <63750622+agnerau@users.noreply.github.com>
2026-05-05 16:58:49 +0300

    add height handler

---
+++ internal/handlers/height_handler.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"goplants/internal"
+	"goplants/internal/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HeightHandler struct {
+	Service *services.HeightService
+}
+
+func (h *HeightHandler) CreateHeight(c *gin.Context) {
+	var height internal.Height
+
+	if err := c.ShouldBindJSON(&height); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	plantID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid plant id"})
+		return
+	}
+
+	height.PlantID = plantID
+
+	err = h.Service.CreateHeight(&height)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusCreated)
+}
+
+func (h *HeightHandler) GetHeights(c *gin.Context) {
+	plantID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "invalid plant id"})
+		return
+	}
+	heights, err := h.Service.GetHeights(plantID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, heights)
+}
