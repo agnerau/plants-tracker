da3062375beb436fe8d09c97b92826507c8d6c86
Agne Rauluseviciute <63750622+agnerau@users.noreply.github.com>
2026-05-07 14:39:59 +0300

    add redis cache init for heights

--- internal/app/height.go
+++ internal/app/height.go
@@ -5,10 +5,12 @@ import (
 	"goplants/internal/handlers"
 	"goplants/internal/repositories"
 	"goplants/internal/services"
+
+	"github.com/redis/go-redis/v9"
 )
 
-func InitHeightHandler(db *sql.DB) *handlers.HeightHandler {
+func InitHeightHandler(db *sql.DB, rdb *redis.Client) *handlers.HeightHandler {
 	repo := &repositories.HeightRepository{DB: db}
-	service := &services.HeightService{Repo: repo}
+	service := &services.HeightService{Repo: repo, RDB: rdb}
 	return &handlers.HeightHandler{Service: service}
 }
--- internal/services/height_service.go
+++ internal/services/height_service.go
@@ -3,10 +3,13 @@ package services
 import (
 	"goplants/internal"
 	"goplants/internal/repositories"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type HeightService struct {
 	Repo *repositories.HeightRepository
+	RDB  *redis.Client
 }
 
 func (s *HeightService) CreateHeight(h *internal.Height) error {
